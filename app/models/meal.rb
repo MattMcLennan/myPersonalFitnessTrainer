4cda2fa10777a86a2e3d69711fd3e6a2578d2e1c
MattMcLennan <mclennan6891@gmail.com>
2015-11-05 20:55:18 +0000

    Added calculated calories, protein, carbs and fat based on recommended grams

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -15,41 +15,39 @@ class Meal < ActiveRecord::Base
     snack2_target = calories_target * 0.075
     dinner_target = calories_target * 0.25
     snack3_target = calories_target * 0.05
-    # snack1_target=[]
-    # lunch_target=[]
-    # snack2_target=[]
-    # dinner_target=[]
-    # snack3_target=[]
 
-    # Format for all targets is array with [cals, protein, carbs, fat]
-    # nutrition_target.each do |nutrition_category|
-    #   breakfast_target.push(nutrition_category * 0.25)
-    #   # snack1_target.push(nutrition_category * 0.075)
-    #   # lunch_target.push(nutrition_category * 0.20)
-    #   # snack2_target.push(nutrition_category * 0.075)
-    #   # dinner_target.push(nutrition_category * 0.25)
-    #   # snack3_target.push(nutrition_category * 0.05)
-    # end
 
     breakfast_list = Meal.where("category LIKE ?","Breakfast")
     breakfast_plan = {
-      items1: {
+      item1: {
         name: breakfast_list[0].food,
         grams: ((breakfast_list[0].per_grams / breakfast_list[0].calories.to_f) * (breakfast_target * 0.80)).round(-1)
-        # calories: lambda { (breakfast_list[0].calories / breakfast_list[0].per_grams) * breakfast_plan[:calories]}
-        # protein: lambda { (breakfast_list[0].calories / breakfast_list[0].per_grams) * breakfast_plan[:calories]}
-        # carbs:  lambda { (breakfast_list[0].calories / breakfast_list[0].per_grams) * breakfast_plan[:calories]}
-        # fat:  lambda { (breakfast_list[0].calories / breakfast_list[0].per_grams) * breakfast_plan[:calories]}
       }
-      # item2: {
-      #   name: breakfast_list[1].food,
-      #   grams: (breakfast_list[1].calories / breakfast_list[1].per_grams) * (breakfast_target * 0.2),
-      #   calories: calories_target * breakfast_target
-      # }
+      item2: {
+        name: breakfast_list[1].food,
+        grams: (breakfast_list[1].calories / breakfast_list[1].per_grams) * (breakfast_target * 0.2),
+      }
     }
 
-    # :hello => lambda { "Hello #{foo[:world]}" }}
+    # Populating item1 breakfast
+    breakfast_plan[:item1][:calories] = ((breakfast_list[0].calories / breakfast_list[0].per_grams.to_f) * breakfast_plan[:item1][:grams]).round(-1)
+    breakfast_plan[:item1][:protein] = ((breakfast_list[0].protein / breakfast_list[0].per_grams.to_f) * breakfast_plan[:item1][:grams]).round(-1)
+    breakfast_plan[:item1][:carbs] = ((breakfast_list[0].carbs / breakfast_list[0].per_grams.to_f) * breakfast_plan[:item1][:grams]).round(-1)
+    breakfast_plan[:item1][:fat] = ((breakfast_list[0].fat / breakfast_list[0].per_grams.to_f) * breakfast_plan[:item1][:grams]).round(-1)
 
-    binding.pry
+    # Populating item2 breakfast
+    breakfast_plan[:items1][:calories] = ((breakfast_list[0].calories / breakfast_list[0].per_grams.to_f) * breakfast_plan[:items1][:grams]).round(-1)
+    breakfast_plan[:items1][:protein] = ((breakfast_list[0].protein / breakfast_list[0].per_grams.to_f) * breakfast_plan[:items1][:grams]).round(-1)
+    breakfast_plan[:items1][:carbs] = ((breakfast_list[0].carbs / breakfast_list[0].per_grams.to_f) * breakfast_plan[:items1][:grams]).round(-1)
+    breakfast_plan[:items1][:fat] = ((breakfast_list[0].fat / breakfast_list[0].per_grams.to_f) * breakfast_plan[:items1][:grams]).round(-1)
+
+
+
+
+
+
+
+
+
   end
 end
