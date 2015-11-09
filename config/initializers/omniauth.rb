6822b037fe4480a42b78f44c501d1271b17c176c
Andres Cisneros <andres.cisneros3@gmail.com>
2015-11-09 16:10:29 +0000

    reset fitbit api

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :fitbit, 'f03b54e47ba1660519ced49131fd7ff9', 'a3d5288ecd685c7ee24cacfcdea1a5a3'
+  provider :fitbit, 'd73fc36ebb2877cb1900ad5b34630227', '7c52a7bec1d9dcabe7e7b0bfab0bde2a'
 end
 
-# https://github.com/tkgospodinov/omniauth-fitbit
+# https://github.com/tkgospodinov/omniauth-
+
