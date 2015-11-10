ba5d844bb6da1a1548d5e9715ea55c1173c902ed
MattMcLennan <mclennan6891@gmail.com>
2015-11-10 01:51:11 +0000

    Made current_user helper method public

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  protected
-
   def current_user
     @current_user ||=User.find(session[:user_id]) if session[:user_id]
-    return @current_user
   end
 
   def signed_in?
