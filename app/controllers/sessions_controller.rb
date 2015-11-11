eeb957d30f02e8b8a0b2a78b276e23a80b34823b
Andres Cisneros <andres.cisneros3@gmail.com>
2015-11-11 22:36:59 +0000

    user gender and weight being pulled from fitbit api now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,19 +10,11 @@ class SessionsController < ApplicationController
     @user.provider = auth["provider"]
     @user.uid = auth["uid"]
     @user.name = auth["info"]["full_name"]
+    @user.gender = auth["info"]["gender"]
+    @user.weight = auth["extra"]["raw_info"]["user"]["weight"]
     @user.token = auth['credentials'].token
     @user.secret = auth['credentials'].secret
     @user.save
-
-    # @consumer_key = '10c780512cf30750c716e8523c718155'
-    # @consumer_secret = '6b53840b9c9113f7a45eb514b5eb6e68'
-
-    # @client = Fitgem::Client.new({
-    #   :consumer_key => @consumer_key,
-    #   :consumer_secret => @consumer_secret,
-    #   :token => @user.token,
-    #   :secret => @user.secret,
-    #   :user_id => @user.uid})
 
     session[:user_id] = @user.id
 
@@ -41,6 +33,8 @@ class SessionsController < ApplicationController
       :user_id => current_user.uid})
 
     @data = {
+      "average_calories" => client.data_by_time_range('/activities/log/calories', {:base_date => 1.week.ago.to_date.to_s, :end_date => "today"}),
+      "body_weight_goal" => client.body_weight_goal,
       "body_weight" => client.data_by_time_range('/body/weight', {:base_date => "2015-07-07", :end_date => "today"}),
       "steps" => client.data_by_time_range('/activities/log/steps', {:base_date => "2015-07-07", :end_date => "today"}),
       "distance" => client.data_by_time_range('/activities/log/distance', {:base_date => "2015-07-07", :end_date => "today"}),
@@ -49,6 +43,7 @@ class SessionsController < ApplicationController
     }
       # client.activity_on_date_range(:calories, '2015-07-07', 'today') gets total calories out per day
       # client.activity_on_date_range(:steps, '2015-07-07', 'today')  gets total steps out per day
+    
     render :json => @data
   end
 
