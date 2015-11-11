50f600cc7553037558f5ec017181429170909e23
Matthew McLennan <mclennan6891@gmail.com>
2015-11-11 22:18:49 +0000

    Removed unused columns from users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110034837) do
+ActiveRecord::Schema.define(version: 20151111220904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,12 +60,7 @@ ActiveRecord::Schema.define(version: 20151110034837) do
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password_digest"
     t.string   "goal"
-    t.string   "fitBit_ID"
-    t.string   "fitBit_auth"
-    t.string   "fatSecret_ID"
-    t.string   "fatSecret_auth"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "uid"
