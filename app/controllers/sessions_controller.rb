540428bcb428307fd3d8e72f3a3f52c42304fd3b
Matthew McLennan <mclennan6891@gmail.com>
2015-11-13 17:01:27 +0000

    Added ability for user to refrsh the page and see the same daily menu

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,6 +12,7 @@ class SessionsController < ApplicationController
     @user.name = auth["info"]["full_name"]
     @user.token = auth['credentials'].token
     @user.secret = auth['credentials'].secret
+    @user.daily_meal = daily_meal(@user)
     @user.save
 
     session[:user_id] = @user.id
@@ -32,10 +33,10 @@ class SessionsController < ApplicationController
 
     # Error we are getting back meal as null when refreshed page and on same date
 
-    if current_user.updated_at == current_user.created_at
-      meal = JSON.parse(daily_meal)
-    elsif current_user.updated_at.to_date != Date.current
-      meal = JSON.parse(daily_meal)
+    # if current_user.updated_at == current_user.created_at
+    #   meal = JSON.parse(daily_meal)
+    if current_user.updated_at.to_date != Date.current
+      current_user.daily_meal = daily_meal(current_user)
     end
 
     @data = {
@@ -43,15 +44,16 @@ class SessionsController < ApplicationController
       "steps" => client.data_by_time_range('/activities/log/steps', {:base_date => "2015-07-07", :end_date => "today"}),
       "distance" => client.data_by_time_range('/activities/log/distance', {:base_date => "2015-07-07", :end_date => "today"}),
       "calories" => client.data_by_time_range('/activities/log/calories', {:base_date => "2015-07-07", :end_date => "today"}),
-      "meal" => current_meal
+      "meal" => JSON.parse(current_user.daily_meal)
     }
 
     current_user.save!
     render :json => @data
+    binding.pry
   end
 
-  def daily_meal
-    current_user.daily_meal = Meal.algo
+  def daily_meal(user)
+    user.daily_meal = Meal.algo
   end
 
 end
