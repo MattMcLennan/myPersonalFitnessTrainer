aa4a07cc11b3ef5f45410678a5c44ad104c73a53
Matthew McLennan <mclennan6891@gmail.com>
2015-11-15 20:39:11 +0000

    Commented out unneeded meals controller actions

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -5,20 +5,20 @@ class MealsController < ApplicationController
     @meals = JSON.parse(Meal.algo(current_user))
   end
 
-  def new
-    @meal = Meal.new
-  end
+  # def new
+  #   @meal = Meal.new
+  # end
 
-  def destroy
-    @meal.destroy
-  end
+  # def destroy
+  #   @meal.destroy
+  # end
 
-  private
-    def set_meal
-      @meal = Meal.find(params[:id])
-    end
+  # private
+  #   def set_meal
+  #     @meal = Meal.find(params[:id])
+  #   end
 
-    def meal_params
-      params.require(:meal).permit(:food_name, :calories, :protien, :fat, :carbs)
-    end
+  #   def meal_params
+  #     params.require(:meal).permit(:food_name, :calories, :protien, :fat, :carbs)
+  #   end
 end
