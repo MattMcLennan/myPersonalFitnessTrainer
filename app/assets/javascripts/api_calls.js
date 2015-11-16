bde316d12ff65dda624ebb64f87efa52d0928260
Andres Cisneros <andres.cisneros3@gmail.com>
2015-11-16 00:09:59 +0000

    calorie_chart now tracking user data

--- app/assets/javascripts/api_calls.js
+++ app/assets/javascripts/api_calls.js
@@ -19,6 +19,7 @@ $( document ).ready(function() {
 });
 
 function calorie_chart(cals_in, cals_out) {
+   dark_theme();
   $('#container2').highcharts({
 
       chart: {
@@ -41,12 +42,12 @@ function calorie_chart(cals_in, cals_out) {
           outerRadius: '109%'
         }, {
           backgroundColor: {
-            linearGradient: {
-              x1: 0,
-              y1: 0,
-              x2: 0,
-              y2: 1
-            },
+            // linearGradient: {
+            //   x1: 0,
+            //   y1: 0,
+            //   x2: 0,
+            //   y2: 1
+            // },
             stops: [
               [0, '#ffffff'],
               [1, '#ffffff']
@@ -67,7 +68,7 @@ function calorie_chart(cals_in, cals_out) {
       // the value axis
       yAxis: {
         min: 0,
-        max: 3000,
+        max: cals_out + 250 + 400,
 
         minorTickInterval: '1',
         minorTickWidth: 1,
@@ -75,10 +76,10 @@ function calorie_chart(cals_in, cals_out) {
         minorTickPosition: 'inside',
         minorTickColor: '#666',
 
-        tickPixelInterval: 100,
-        tickWidth: 2,
+        tickPixelInterval: 0,
+        tickWidth: 0,
         tickPosition: 'inside',
-        tickLength: 10,
+        tickLength: 0,
         tickColor: '#666',
         labels: {
           step: 2,
@@ -89,24 +90,24 @@ function calorie_chart(cals_in, cals_out) {
         },
         plotBands: [{
           from: 0,
-          to: 1000,
+          to: cals_out -250,
           color: '#fad60a ' // yellow
         }, {
-          from: cals_in - 250,
+          from: cals_out - 250,
           to: cals_out + 250,
           color: '#b0e52c' // green
         }, {
-          from: 2000,
-          to: 3000,
+          from: cals_out + 250,
+          to: cals_out + 400 + 250,
           color: '#fe951b ' // red
         }]
       },
 
       series: [{
-        name: 'Speed',
-        data: [2000],
+        name: 'Calories In',
+        data: [cals_in],
         tooltip: {
-          valueSuffix: ' km/h'
+          valueSuffix: ''
         }
       }]
 
@@ -114,19 +115,216 @@ function calorie_chart(cals_in, cals_out) {
     // Add some life
     function(chart) {
       if (!chart.renderer.forExport) {
-        setInterval(function() {
-          var point = chart.series[0].points[0],
-            newVal,
-            inc = Math.round((Math.random() - 0.5) * 20);
-
-          newVal = point.y + inc;
-          if (newVal < 0 || newVal > 200) {
-            newVal = point.y - inc;
-          }
-
-          point.update(newVal);
-
-        }, 3000);
+          var point = chart.series[0].points[0];
       }
     });
+}
+
+// Load the fonts
+function dark_theme() {
+   Highcharts.createElement('link', {
+      href: '//fonts.googleapis.com/css?family=Unica+One',
+      rel: 'stylesheet',
+      type: 'text/css'
+   }, null, document.getElementsByTagName('head')[0]);
+
+   Highcharts.theme = {
+      colors: ["#2b908f", "#90ee7e", "#f45b5b", "#7798BF", "#aaeeee", "#ff0066", "#eeaaee",
+         "#55BF3B", "#DF5353", "#7798BF", "#aaeeee"],
+      chart: {
+         backgroundColor: {
+            linearGradient: { x1: 0, y1: 0, x2: 0, y2: 0 },
+            stops: [
+               [0, '#2a2a2b'],
+               [1, '#3e3e40']
+            ]
+         },
+         style: {
+            fontFamily: "'Unica One', sans-serif"
+         },
+         plotBorderColor: '#606063'
+      },
+      title: {
+         style: {
+            color: '#E0E0E3',
+            textTransform: 'uppercase',
+            fontSize: '20px'
+         }
+      },
+      subtitle: {
+         style: {
+            color: '#E0E0E3',
+            textTransform: 'uppercase'
+         }
+      },
+      xAxis: {
+         gridLineColor: '#707073',
+         labels: {
+            style: {
+               color: '#E0E0E3'
+            }
+         },
+         lineColor: '#707073',
+         minorGridLineColor: '#505053',
+         tickColor: '#707073',
+         title: {
+            style: {
+               color: '#A0A0A3'
+
+            }
+         }
+      },
+      yAxis: {
+         gridLineColor: '#707073',
+         labels: {
+            style: {
+               color: '#E0E0E3'
+            }
+         },
+         lineColor: '#707073',
+         minorGridLineColor: '#505053',
+         tickColor: '#707073',
+         tickWidth: 1,
+         title: {
+            style: {
+               color: '#A0A0A3'
+            }
+         }
+      },
+      tooltip: {
+         backgroundColor: 'rgba(0, 0, 0, 0.85)',
+         style: {
+            color: '#F0F0F0'
+         }
+      },
+      plotOptions: {
+         series: {
+            dataLabels: {
+               color: '#B0B0B3'
+            },
+            marker: {
+               lineColor: '#333'
+            }
+         },
+         boxplot: {
+            fillColor: '#505053'
+         },
+         candlestick: {
+            lineColor: 'white'
+         },
+         errorbar: {
+            color: 'white'
+         }
+      },
+      legend: {
+         itemStyle: {
+            color: '#E0E0E3'
+         },
+         itemHoverStyle: {
+            color: '#FFF'
+         },
+         itemHiddenStyle: {
+            color: '#606063'
+         }
+      },
+      credits: {
+         style: {
+            color: '#666'
+         }
+      },
+      labels: {
+         style: {
+            color: '#707073'
+         }
+      },
+
+      drilldown: {
+         activeAxisLabelStyle: {
+            color: '#F0F0F3'
+         },
+         activeDataLabelStyle: {
+            color: '#F0F0F3'
+         }
+      },
+
+      navigation: {
+         buttonOptions: {
+            symbolStroke: '#DDDDDD',
+            theme: {
+               fill: '#505053'
+            }
+         }
+      },
+
+      // scroll charts
+      rangeSelector: {
+         buttonTheme: {
+            fill: '#505053',
+            stroke: '#000000',
+            style: {
+               color: '#CCC'
+            },
+            states: {
+               hover: {
+                  fill: '#707073',
+                  stroke: '#000000',
+                  style: {
+                     color: 'white'
+                  }
+               },
+               select: {
+                  fill: '#000003',
+                  stroke: '#000000',
+                  style: {
+                     color: 'white'
+                  }
+               }
+            }
+         },
+         inputBoxBorderColor: '#505053',
+         inputStyle: {
+            backgroundColor: '#333',
+            color: 'silver'
+         },
+         labelStyle: {
+            color: 'silver'
+         }
+      },
+
+      navigator: {
+         handles: {
+            backgroundColor: '#666',
+            borderColor: '#AAA'
+         },
+         outlineColor: '#CCC',
+         maskFill: 'rgba(255,255,255,0.1)',
+         series: {
+            color: '#7798BF',
+            lineColor: '#A6C7ED'
+         },
+         xAxis: {
+            gridLineColor: '#505053'
+         }
+      },
+
+      scrollbar: {
+         barBackgroundColor: '#808083',
+         barBorderColor: '#808083',
+         buttonArrowColor: '#CCC',
+         buttonBackgroundColor: '#606063',
+         buttonBorderColor: '#606063',
+         rifleColor: '#FFF',
+         trackBackgroundColor: '#404043',
+         trackBorderColor: '#404043'
+      },
+
+      // special colors for some of the
+      legendBackgroundColor: 'rgba(0, 0, 0, 0.5)',
+      background2: '#505053',
+      dataLabelsColor: '#B0B0B3',
+      textColor: '#C0C0C0',
+      contrastTextColor: '#F0F0F3',
+      maskColor: 'rgba(255,255,255,0.3)'
+   };
+   Highcharts.setOptions(Highcharts.theme)
 }
